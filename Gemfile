32201fdd0d1bfcd39737e480a281135d4df934cc
Ela Passarelli <epassarelli18@hb.edu>
2016-09-26 12:17:40 +0000

    byela

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+gem 'pry'
+gem 'sinatra'
+gem 'shotgun'
+gem 'twilio-ruby', '~> 4.11.1'
+gem 'heroku'
---
+++ application_controller.rb
@@ -0,0 +1,9 @@
+require 'bundler'
+Bundler.require
+require 'rubygems' # not necessary with ruby 1.9 but included for completeness
+
+
+
+class ApplicationController < Sinatra::Base
+
+  end
