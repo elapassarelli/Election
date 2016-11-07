e0add4b7bd18b649c5ec4ef5920df20e3ecb330f
Ela Passarelli <epassarelli18@hb.edu>
2016-11-07 03:08:42 +0000

    yay

--- models/CVote.rb
+++ models/CVote.rb
@@ -1,6 +1,14 @@
 
 def can_vote(name, array)
-  if array.include?(name) == false
+  if array.include?(name) == true
+    return true
+  else
+    return false
+  end
+end
+
+def is_admin(key)
+  if key == "blazers"
     return true
   else
     return false
